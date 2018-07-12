5345e239520228f2bbf67f2702f4858485d0662a
velkonost <velkonost@gmail.com>
2018-07-12 17:39:33 +0600

    added view

--- Weights/ScaleView.swift
+++ Weights/ScaleView.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
+@IBDesignable
 class ScaleView: UIView {
 
     override func draw(_ rect: CGRect) {
-		// let context = UIGraphicsGetCurrentContext() // 1
-		//context?.setStrokeColor(UIColor.red.cgColor) // 2
-//		context?.setLineWidth(14.0) // 3
-//		context?.addArc(center: CGPoint(x: 375 / 2, y: 375 / 2), radius: 375 / 2 - 14, startAngle: 0, endAngle: 2 * CGFloat(M_PI), clockwise: true) // 4
-//		context?.strokePath() // 5
-		
 		let context = UIGraphicsGetCurrentContext()
+		context?.setStrokeColor(UIColor.red.cgColor)
+		context?.setLineWidth(14.0)
+		context?.addArc(center: CGPoint(x: 375 / 2, y: 375 / 2), radius: 375 / 2 - 14, startAngle: 0, endAngle: 2 * CGFloat(Double.pi), clockwise: true)
+		context?.strokePath()
+		
+		context?.setLineWidth(1.0)
+		context?.setStrokeColor(UIColor.lightGray.cgColor)
+		context?.addArc(center: CGPoint(x: 375 / 2, y: 375 / 2), radius: 375 / 4 - 14, startAngle: 0, endAngle: 2 * CGFloat(Double.pi), clockwise: true)
+		context?.strokePath()
     }
 	
 
--- Weights/ViewController.swift
+++ Weights/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var scaleView: ScaleView!
+	@IBOutlet weak var forceLabel: UILabel!
+	@IBOutlet weak var grammLabel: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
