56873bd86ded7db74c16189acb9c47269361a643
velkonost <velkonost@gmail.com>
2018-07-12 15:12:56 +0600

    initial commit

---
+++ Weights/ScaleView.swift
@@ -0,0 +1,24 @@
+//
+//  ScaleView.swift
+//  Weights
+//
+//  Created by Artem Klimenko on 12.07.18.
+//  Copyright © 2018 velkonost. All rights reserved.
+//
+
+import UIKit
+
+class ScaleView: UIView {
+
+    override func draw(_ rect: CGRect) {
+		// let context = UIGraphicsGetCurrentContext() // 1
+		//context?.setStrokeColor(UIColor.red.cgColor) // 2
+//		context?.setLineWidth(14.0) // 3
+//		context?.addArc(center: CGPoint(x: 375 / 2, y: 375 / 2), radius: 375 / 2 - 14, startAngle: 0, endAngle: 2 * CGFloat(M_PI), clockwise: true) // 4
+//		context?.strokePath() // 5
+		
+		let context = UIGraphicsGetCurrentContext()
+    }
+	
+
+}
