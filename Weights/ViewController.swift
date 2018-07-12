17b6cfd99eab5b7105d102a99dd770cf81888285
velkonost <velkonost@gmail.com>
2018-07-12 17:48:10 +0600

    added main functional

--- Weights/ViewController.swift
+++ Weights/ViewController.swift
@@ -7,22 +7,54 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+	
 	@IBOutlet weak var scaleView: ScaleView!
 	@IBOutlet weak var forceLabel: UILabel!
 	@IBOutlet weak var grammLabel: UILabel!
+	
+	var isPlaySound = true
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		forceLabel.text = "0% force"
+		grammLabel.text = "0 gram"
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if let touch = touches.first {
+			if #available(iOS 9.0, *) {
+				if traitCollection.forceTouchCapability == UIForceTouchCapability.available {
+					if touch.force >= touch.maximumPossibleForce {
+						forceLabel.text = "100% force"
+						grammLabel.text = "385 gram"
+					} else {
+						let force = (touch.force / touch.maximumPossibleForce) * 100
+						let grams = force * 385 / 100
+						let roundGrams = Int(grams)
+						
+						forceLabel.text = "\(Int(force))% force"
+						grammLabel.text = "\(roundGrams) gram"
+					}
+				}
+			}
+		}
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		forceLabel.text = "0% force"
+		grammLabel.text = "0 gram"
+	}
+		
+		
 }
 
